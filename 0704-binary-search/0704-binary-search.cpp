85069533b21284f55fdf47d6e262de6bb1fb3692
Muhammad Ammar <141748738+muhammadammar2@users.noreply.github.com>
2024-05-25 22:35:09 +0500

    Time: 24 ms (76.78%), Space: 30 MB (21.60%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,11 +1,33 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for (int i=0; i<nums.size(); i++) {
-            if (nums[i] == target) {
-                return i;
+         //     just kidding this is the worst code i've ever written in my entire life {linear}
+    //     for (int i=0; i<nums.size(); i++) {
+    //         if (nums[i] == target) {
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
+        
+        int left =0;
+        int right = nums.size()-1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) /2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if(nums[mid] < target) {
+                left = mid +1;
+            }
+            else {
+            right = mid-1;
             }
         }
-        return -1;
+        
+           return -1;    
+        
     }
 };
