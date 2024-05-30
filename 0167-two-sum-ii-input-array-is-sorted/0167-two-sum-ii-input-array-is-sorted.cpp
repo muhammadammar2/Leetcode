202f937c8427fd87c82e99bd9410ae68dc9da92f
Muhammad Ammar <141748738+muhammadammar2@users.noreply.github.com>
2024-05-30 10:18:02 +0500

    Time: 14 ms (18.24%), Space: 17.8 MB (91.52%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,20 +1,19 @@
-//2 ptr approach bcz loops would give O(n^2).
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int left =0;
+        int left = 0;
         int right = numbers.size()-1;
         
-        while (left<right){
-            int sum = numbers[left]+ numbers [right];
-            if (sum == target) return {left+1, right+1};
-            else if (sum<target) {
-                left++;
-            }
+        while (left < right){
+            int currSum = numbers[left] + numbers[right];
+            
+            if (currSum < target) left+= 1;
+            else if (currSum > target) right-=1;
             else {
-                right--;
+                return vector<int> {left+1 , right+1};
             }
+          
         }
-        return {};
+        return vector<int> {};
     }
 };
