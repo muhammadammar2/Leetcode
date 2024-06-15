90edb88d13769ad36b48d0b55db16f7a386cf82a
Muhammad Ammar <141748738+muhammadammar2@users.noreply.github.com>
2024-06-15 10:09:23 +0500

    Time: 7 ms (75.99%), Space: 9.6 MB (33.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       unordered_map<char , int> freqS;
-       unordered_map<char , int> freqT;
-       for (int ss: s){
-        freqS[ss]++;
-       }
-       for (int tt : t){
-        freqT[tt]++;
-       }
-       return freqS==freqT;
+        unordered_map <char , int> freqS;
+        unordered_map <char , int> freqT;
+        
+        for (char ss : s) {
+            freqS[ss]++;
+        }
+        for (char tt : t) {
+            freqT[tt]++;
+        }
+        return freqS == freqT;
     }
 };
