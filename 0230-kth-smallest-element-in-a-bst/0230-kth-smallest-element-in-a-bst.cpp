ca4470c4f127fa1fbe8cf0af9adc32456ce2ca74
Muhammad Ammar <141748738+muhammadammar2@users.noreply.github.com>
2024-06-26 16:22:07 +0500

    Time: 10 ms (80.27%), Space: 22.7 MB (37.50%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -9,27 +9,57 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
     //dfs reccursive
-public:
-    int kthSmallest(TreeNode* root, int k) {
-     int count =0;
-        int result = 0;
-        inOrder(root , count , k , result);
-        return result;
-    }
-private: 
-    void inOrder (TreeNode* node, int& count, int k , int& result) {
-        if (!node) return;
-        //visiting left
-        inOrder (node->left , count , k , result);
+
+// class Solution {
+// public:
+//     int kthSmallest(TreeNode* root, int k) {
+//      int count =0;
+//         int result = 0;
+//         inOrder(root , count , k , result);
+//         return result;
+//     }
+// private: 
+//     void inOrder (TreeNode* node, int& count, int k , int& result) {
+//         if (!node) return;
+//         //visiting left
+//         inOrder (node->left , count , k , result);
         
-        count++;
-        if (count == k) {
-            result = node->val;
-            return;
+//         count++;
+//         if (count == k) {
+//             result = node->val;
+//             return;
+//         }
+//         //visiting right
+//         inOrder (node->right , count , k , result);
+//     }
+// };
+
+   //dfs iterative
+
+class Solution {
+public: 
+    int kthSmallest(TreeNode* root , int k) {
+    stack <TreeNode*> s;
+        TreeNode* node = root;
+        int count = 0;
+        
+        while (!s.empty() || node!=NULL) {
+            while (node!=NULL) {
+                s.push(node);
+                node = node->left; //now at left
+            }
+            //node must be the leftmost node
+            node = s.top();
+            s.pop();
+            
+            if (++count == k) {
+                return node->val;
+            }
+            node = node->right;
         }
-        //visiting right
-        inOrder (node->right , count , k , result);
+        return -1; 
     }
+    
 };
