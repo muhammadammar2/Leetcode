143270b609176cd905d4869da1f49cc16f92ac1e
Muhammad Ammar <141748738+muhammadammar2@users.noreply.github.com>
2024-06-29 15:51:16 +0500

    Time: 0 ms (100.00%), Space: 14.2 MB (27.46%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -16,20 +16,18 @@ public:
         if (!root) return result;
         queue <TreeNode*> q;
         q.push(root);
-        
-        while (!q.empty()){
+        while (!q.empty()) {
             int level = q.size();
-            
-            for (int i =0; i<level; i++) {
+            for (int i =0; i< level; i++) {
                 TreeNode* node = q.front();
                 q.pop();
-                
-                if (i == level-1 ) result.push_back(node->val);
-                
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
+               if (i == level -1) result.push_back(node->val) ;
+            
+            if (node->left) q.push(node->left);
+            if (node->right) q.push(node->right);
         }
+        }
+        
         return result;
-    }
+        }
 };
