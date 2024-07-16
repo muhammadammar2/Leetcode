9043b9a4cc55bf56e3ce3b0183f725168bc7b4a7
Muhammad Ammar <141748738+muhammadammar2@users.noreply.github.com>
2024-07-16 21:40:57 +0500

    Time: 0 ms (100.00%), Space: 9.6 MB (6.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
+//         int n = nums.size();
+//         if (n == 0) return 0;
+//         if (n == 1) return nums[0];
         
-        vector <int> dp(n);
-        dp[0] = nums[0];
-        dp[1] = max (dp[0] , nums[1]);
+//         vector <int> dp(n);
+//         dp[0] = nums[0];
+//         dp[1] = max (dp[0] , nums[1]);
         
-        for (int i = 2; i<n; ++i) {
-            dp[i] = max(dp[i-1] , nums[i] + dp[i - 2]);
-        }
-        return dp[n-1];
+//         for (int i = 2; i<n; ++i) {
+//             dp[i] = max(dp[i-1] , nums[i] + dp[i - 2]);
+//         }
+//         return dp[n-1];
+        
+       int n = nums.size();
+       vector <int> memo (n , -1);
+
+        return hF (n - 1, nums , memo) ;
+    }
+private:
+    int hF (int i , vector <int>& nums , vector <int>& memo) {
+        if (i < 0) return 0;
+        if (i == 0) return nums[0];
+        if (i == 1) return max (nums[0] , nums[1]);
+        
+        if (memo[i] != -1) return memo[i];
+        memo[i] = max (nums[i] + hF (i -2 , nums , memo) , hF(i -1 , nums , memo));
+        return memo[i];
     }
 };
