560d3e851b6abeaa0d632f4f53596e2ed276bd1b
Muhammad Ammar <141748738+muhammadammar2@users.noreply.github.com>
2024-07-20 14:42:35 +0500

    Time: 23 ms (21.52%), Space: 26.1 MB (9.11%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector <unordered_set<char>> rows(9); 
-        vector <unordered_set<char>> cols(9);
-        vector <unordered_set<char>> boxes(9);
-
-        for (int i=0; i<9; i++){
-            for (int j=0; j<9; j++){
-                int num = board[i][j];
-                if (num == '.') continue;
+        vector <unordered_set <int>> rows(9);
+        vector <unordered_set <int>> cols(9);
+        vector <unordered_set <int>> boxes(9);
+        
+        for (int i =0; i<9; ++i) {
+            for (int j =0; j<9; ++j) {
+                int nums = board[i][j];
+                if (nums == '.') continue;
                 
                 int boxIndex = (i/3)*3+(j/3);
                 
-                if(rows[i].count(num) || cols[j].count(num) || boxes[boxIndex].count(num)) return false;
-                rows[i].insert(num);
-                cols[j].insert(num);
-                boxes[boxIndex].insert(num);
+                if (rows[i].count(nums) || cols[j].count(nums) || boxes[boxIndex].count(nums)) return false;
+                
+                rows[i].insert(nums);
+                cols[j].insert(nums);
+                boxes[boxIndex].insert(nums);
             }
         }
-           return true;
+        return true;
     }
 };
