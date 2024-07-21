3b5485f917762654c904f3b582321292870190b3
Muhammad Ammar <141748738+muhammadammar2@users.noreply.github.com>
2024-07-21 16:35:27 +0500

    Time: 61 ms (70.04%), Space: 27.5 MB (57.25%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,42 +1,28 @@
-// sort
-// iterate through array for a fixed number then skip duplicates
-// use 2 ptrs and skip duplicates
-// T.C = O(nlogn) + O(n) + O(n) = O(n2)
-
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector <vector<int>>result;
-        sort(nums.begin(), nums.end());
-        
-        for (int i=0; i<nums.size()-2 ; ++i) {
-            if(i > 0 && nums[i] == nums[i-1]) continue; //skipping dublicates
+        vector <vector <int>> res;
+        sort (nums.begin() , nums.end());
+        for (int i =0; i<nums.size()-2; ++i) {
+            if (i > 0 && nums[i] == nums[i -1]) continue;
             
-            int left = i+1;
+            int left = i + 1;
             int right = nums.size()-1;
             
-            while(left < right){
+            while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                
-                if(sum == 0) {
-                    result.push_back({nums[i] , nums[left] , nums[right]});
-                    while (left < right && nums[left] == nums[left +1]) ++left;  //skipping dublicates
-                    while (left < right && nums[right] == nums[right-1]) --right; //skipping dublicates
+                if (sum == 0) {
+                    res.push_back({nums[i] , nums[left] , nums[right]});
+                    while (left < right && nums[left] == nums[left + 1]) left ++;
+                    while (left < right && nums[right] == nums[right - 1]) right --;
                     
-                    ++left;
-                    --right;
-                    
-                   
+                    left ++;
+                    right --;
                 }
-                 else if(sum < 0) {
-                        ++left;
-                    }
-                    else {
-                        --right;
-                    }
+               else if (sum < 0) left ++;
+                else right --;
             }
-}
-                    return result;
-
+        }
+        return res;
     }
 };
