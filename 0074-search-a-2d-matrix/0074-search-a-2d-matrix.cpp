616faf8ab3e7738a46a98d85e9ec77bff7eced6e
Muhammad Ammar <141748738+muhammadammar2@users.noreply.github.com>
2024-07-23 16:28:40 +0500

    Time: 7 ms (22.20%), Space: 12.1 MB (23.87%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,27 +1,17 @@
-//matrix = row
-//matrix[0] = col
-//assume it as a 1D array or a sorted list
-//find mid
-//find midVal (matrix)
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if(matrix.empty() || matrix[0].empty()) return 0;
-        int m = matrix.size();
-        int n = matrix[0].size();
         int left = 0;
-        int right = m*n -1;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int right = n * m - 1;
         
-        while(left <= right) {
-            int mid = left + (right - left) /2;
-            int midVal = matrix [mid / n] [mid % n];
+        while (left <= right) {
+            int mid = left + (right - left) / 2; 
+            int midVal = matrix[mid / m][mid % m];
             if (midVal == target) return true;
-            else if (midVal < target) {
-                left = mid+1;
-            }
-            else {
-                right = mid-1;
-            }
+            if (midVal < target) left = mid + 1;
+            else right = mid - 1;
         }
         return false;
     }
