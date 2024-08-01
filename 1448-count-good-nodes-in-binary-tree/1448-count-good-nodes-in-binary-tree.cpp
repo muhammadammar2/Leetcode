93034a2f70a61d93df8ed27762e21c031847ba51
Muhammad Ammar <141748738+muhammadammar2@users.noreply.github.com>
2024-08-01 17:17:00 +0500

    Time: 178 ms (5.21%), Space: 85.1 MB (14.80%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -12,17 +12,23 @@
 class Solution {
 public:
     int goodNodes(TreeNode* root) {
-      return dfs (root , root->val);
-    }
-private:
-    int dfs (TreeNode* node , int maxVal) {
-        if (!node) return 0;
-        int good = 0;
-        if (node->val >= maxVal) {
-            good = 1;
-            maxVal = node->val;
+        if (!root) return 0;
+        stack<pair <TreeNode* , int>>s;
+        s.push({root , root->val});
+        int c = 0;
+        while (s.empty() == false) {
+            auto [node , maxSoF] = s.top();
+            s.pop();
+            if (node->val >= maxSoF) {
+                c++;
+            }
+            if (node->left) {
+                s.push({node->left , max(maxSoF, node->val)});
+            }
+            if (node->right) {
+                s.push({node->right , max(maxSoF, node->val)});
+            }
         }
-        return good + dfs (node->left , maxVal) + dfs (node->right , maxVal);
+        return c;
     }
-    
 };
